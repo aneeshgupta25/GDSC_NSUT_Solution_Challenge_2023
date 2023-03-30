9c1fd4fee55c76dec48b66c5b280167f7af01096
AsCress <anash.cheema@gmail.com>
2023-03-30 23:10:04 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -56,60 +57,75 @@ class _MyHomePageState extends State<MyHomePage> {
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      // called again, and so nothing would appear to happen.S
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      decoration: const BoxDecoration(
+        image: DecorationImage(image: AssetImage('assets/image.svg'), fit: BoxFit.fill)
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      child:Scaffold(
+        body: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Wrap(
+                  direction: Axis.horizontal,
+                  spacing: 10.0,
+                  children: <Widget>[const Padding(
+
+                    padding: EdgeInsets.all(30.0),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                    SizedBox(height: 400),
+                     Padding(padding: EdgeInsets.only(left:30.0, right:30),
+                        child: SizedBox(
+                          height: 55,
+                          child:TextField(
+                            style: TextStyle(fontSize: 12),
+                            decoration: InputDecoration(
+                                border:OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                                hintText: 'Email'
+                            ),
+                        )
+                        )),
+                     Padding(padding: EdgeInsets.all(30.0),
+                        child: SizedBox(
+                          height: 55,
+                          child: TextField(
+                            style: TextStyle(fontSize: 12),
+                            decoration: InputDecoration(
+                                border:OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                                hintText: 'Password'
+                            ),
+                          ),
+                        )),
+                    Padding(padding: const EdgeInsets.all(30.0),
+                        child: SizedBox(
+                            width: double.infinity,
+                            child:ElevatedButton(
+                              style:ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)
+                              )))
+                              ,onPressed:(){}, child: const Padding(padding: EdgeInsets.all(10), child:const Text('Login', style: TextStyle(fontSize: 20),)),
+                            )
+                        )),]
+
+              )
+            ],
+          ),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
